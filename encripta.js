609b2f8ee02b047e4b74fedd87fd4ce486dc921d
Atuesta <54296676+simonpjflor@users.noreply.github.com>
2024-07-15 23:02:56 -0500

    Create encripta.js

---
+++ encripta.js
@@ -0,0 +1,20 @@
+'buttons Js code- desencriptar copiar'
+
+const encriptationKeys = {a:'ai', e:'enter', i:'imes', o:'ober', u:'ufat'}
+
+function main() {
+    const encryptButton = document.getElementById('');
+    encriptarButton.addEventListener('click',encrypt);  
+    const decryptButton = document.getElementById('encriptador__input-desencriptar');
+    desEncriptarButton.addEventListener('click',decrypt); 
+    const copyButton = document.getElementById('');
+    copiarButton.addEventListener('click',copy);
+}
+
+function encrypt(){}
+
+function decrypt(){}
+
+function copy(){}
+
+function alerts(){}
